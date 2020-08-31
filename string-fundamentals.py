9f8fab0f9376cebc396ed39e0f122e4deb9d3066
Kyle Sweigart <kasweigart@gmail.com>
2020-08-31 18:36:51 -0400

    special expression and format method syntax

--- string-fundamentals.py
+++ string-fundamentals.py
@@ -337,5 +337,141 @@ print('{0:>10} = {1:<10}'.format('spam', 123.4567))
 print('{0.platform:>10} = {1[kind]:<10}'.format(sys, dict(kind='laptop')))
 
 
+print('{0:e}, {1:.3e}, {2:g}'.format(3.14159, 3.14159, 3.14159))
+
+print('{0:f}, {1:.2f}, {2:06.2f}'.format(3.14159, 3.14159, 3.14159))
+
+
+print('{0:X}, {1:o}, {2:b}'.format(255, 255, 255)) # Hex, octal, binary
+
+print(bin(255), int('11111111', 2), 0b11111111) # Other to/from binary
+
+print(hex(255), int('FF', 16), 0xFF) # Other to/from hex
+
+print(oct(255), int('377', 8), 0o377) # Other to/from octal, in 3.X. 2.X prints and accepts 0377
+
+
+print('{0:.2f}'.format(1 / 3.0)) # Parameters hardcoded
+
+print('%.2f' % (1 / 3.0)) # Ditto for expression
+
+
+print('{0:.{1}f}'.format(1 / 3.0, 4)) # Take value from arguments
+
+print('%.*f' % (4, 1 / 3.0)) # Ditto for expression
+
+
+print('{0:2f}'.format(1.2345)) # String method
+
+print(format(1.2345, '.2f')) # Built-in function
+
+print('%.2f' % 1.2345) # Expression
+
+
+
+# Comparison to the % Formatting Expression
+
+
+print('%s=%s' % ('spam', 42)) # Format expression: in all 2.X/3.X
+
+print('{0}={1}'.format('spam', 42)) # Format method: in 3.0+ and 2.6+
+
+print('{}={}'.format('spam', 42)) # With autonumbering: in 3.1+ and 2.7
+
+
+print('%s, %s, and %s ' % (3.14, 42, [1, 2])) # Arbitrary types
+
+print('My %(kind)s runs %(platform)s' % {'kind': 'laptop', 'platform': sys.platform}) 
+
+print('My %(kind)s runs %(platform)s' % dict(kind='laptop', platform=sys.platform))
+
+somelist = list('SPAM')
+parts = somelist[0], somelist[-1], somelist[1:3]
+print('first=%s, last=%s, middle=%s' % parts)
+
+
+# Adding specific formatting
+
+print('%-10s = %10s' % ('spam', 123.4567))
+
+print('%10s = %-10s' % ('spam', 123.4567))
+
+print('%(plat)10s = %(kind)-10s' % dict(plat=sys.platform, kind='laptop'))
+
+
+# Floating-point numbers
+
+print('%e, %.3e, %g' % (3.14159, 3.14159, 3.14159))
+
+print('%f, %.2f, %06.2f' % (3.14159, 3.14159, 3.14159))
+
+
+# Hex and octal, but not binary (see ahead)
+
+print('%x, %o' % (255, 255))
+
+
+import sys
+
+print('My {1[kind]:<8} runs {0.platform:>8}'.format(sys, {'kind': 'laptop'}))
+
+print('My %(kind)-8s runs %(plat)8s' % dict(kind='laptop', plat=sys.platform))
+
+
+# Building data ahead of time in both
+
+data = dict(platform=sys.platform, kind='laptop')
+
+print('My {kind:<8} runs {platform:>8}'.format(**data))
+
+print('My %(kind)-8s runs %(platform)8s' % data)
+
+
+print('{0:d}'.format(999999999999))
+
+print('{0:,d}'.format(999999999999))
+
+
+print('{0:,d}'.format(999999999999))
+
+print('{:,d} {:,d}'.format(9999999, 8888888))
+
+print('{:,.2f}'.format(296999.2567))
+
+
+# from formats import commas, money
+
+# print('%s' % commas(999999999999))
+
+# print('%s %s' % commas(9999999), commas(8888888))
+
+# print('%s' % money(296999.2567))
+
+
+# print([commas(x) for x in (9999999, 8888888)])
+
+# print('%s %s' % tuple(commas(x) for x in (9999999, 8888888)))
+
+# print(''.join(commas(x) for x in (9999999, 8888888)))
+
+
+
+# Why the Format Method?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
