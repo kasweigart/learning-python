c5e3bf46858d7dbdeb97557f74f93b2f3cc8532f
Kyle Sweigart <kasweigart@gmail.com>
2020-09-09 17:31:17 -0400

    for loops

--- while-and-for-loops.py
+++ while-and-for-loops.py
@@ -56,3 +56,108 @@ else:
     print(y, 'is prime') # Normal exit
 
 
+for x in ['spam', 'eggs', 'ham']:
+    print(x, end=' ')
+
+
+sum = 0
+
+for x in [1, 2, 3, 4]:
+    sum = sum + x
+
+print(sum)
+
+
+prod = 1
+
+for item in [1, 2, 3, 4]: prod *= item
+
+print(prod)
+
+
+S = 'lumberjack'
+
+T = ('and', "I'm", 'okay')
+
+for x in S: print(x, end=' ') # Iterate over a string
+
+for x in T: print(x, end=' ') # Iterate over a tuple
+
+
+T = [(1,2),(3,4),(5,6)]
+
+for (a,b) in T: # Tuple assignment at work
+    print(a,b)
+
+
+D = {'a': 1, 'b': 2, 'c': 3}
+
+for key in D:
+    print(key, '=>', D[key]) # Use dict keys iterator and index
+
+
+print(list(D.items()))
+
+for (key, value) in D.items():
+    print(key, '=>', value) # Iterate over both keys and values
+
+
+print(T)
+
+for both in T:
+    a, b = both # Manual assignment equivalent
+    print(a, b) # 2.X prints with enclosing tuple '()'
+
+
+((a,b),c) = ((1,2),3) # Nested sequences work too
+
+for ((a,b),c) in [((1,2),3), ((4,5),6)]: print(a,b,c)
+
+
+for ((a,b), c) in [([1,2], 3), ['XY', 6]]: print(a,b,c)
+
+
+a,b,c = (1,2,3) # Tuple assignment
+
+print(a,b,c)
+
+for (a,b,c) in [(1,2,3), (4,5,6)]: # Used in for loop
+    print(a,b,c)
+
+
+a,*b,c = (1,2,3,4) # Extended seq assignment
+
+print(a,b,c)
+
+
+for (a,*b,c) in [(1,2,3,4), (5,6,7,8)]:
+    print(a,b,c)
+
+
+for all in [(1,2,3,4), (5,6,7,8)]: # Manual slicing in 2.X
+    a,b,c = all[0], all[1:3], all[3]
+    print(a,b,c)
+
+
+items = ['aaa', 111, (4,5), 2.01] # A set of objects
+
+tests = [(4,5), 3.14] # Keys to search for
+
+for key in tests: # For all keys
+    for item in items: # For all items
+        if item == key: # Check for match
+            print(key, 'was found')
+            break
+    else:
+        print(key, 'not found!')
+
+
+for key in tests: # For all keys
+    if key in items: # Let Python check for a match
+        print(key, 'was found')
+    else:
+        print(key, 'not found!')
+
+
+
+
